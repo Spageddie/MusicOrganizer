a4b1fb5e7214017ddee8eb5b93178c2b81eaedf4
MattAMarquez <33560698+MattAMarquez@users.noreply.github.com>
2017-11-15 18:52:29 -0500

    Added LinkedINterface
    Added LinkedInterface

--- src/prj5/Class1.java
+++ src/prj5/Class1.java
@@ -7,7 +7,6 @@ public class Class1 {
     }
     
     public static String getMessage() {
-        hjkhubu
         return "Hello world";
     }
 }
---
+++ src/prj5/LinkedInterface.java
@@ -0,0 +1,16 @@
+package prj5;
+
+/**
+ * Interface for the ADT LinkedList
+ * 
+ * @author Matthew Marquez (mmarquez)
+ * @version 2017.11.15
+ * @param <T>
+ *            generic type for LinkedList
+ */
+public interface LinkedInterface<T> {
+    public void clear();
+    public boolean isEmpty();
+    public void add();
+    public T remove();
+}
