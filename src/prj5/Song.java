358457e7f1aff055814fb438796720724faa760f
MattAMarquez <33560698+MattAMarquez@users.noreply.github.com>
2017-11-15 23:06:42 -0500

    Added song class

---
+++ src/prj5/Song.java
@@ -0,0 +1,39 @@
+package prj5;
+
+public class Song {
+    private String title;
+    private String artist;
+    private String year;
+    private String genre;
+    private int index;
+    public Song(String title, String artist, String year, String genre) {
+        this.title = title;
+        this.artist = artist;
+        this.year = year;
+        this.genre = genre;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getArtist() {
+        return artist;
+    }
+    public String getYear() {
+        return year;
+    }
+    public String getGenre() {
+        return genre;
+    }
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+    public void setYear(String year) {
+        this.year = year;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
