29dc5234a76d785df079835bf3c6e400db29b447
MattAMarquez <33560698+MattAMarquez@users.noreply.github.com>
2017-11-15 23:30:18 -0500

    Added data class

---
+++ src/prj5/Data.java
@@ -0,0 +1,13 @@
+package prj5;
+
+public class Data {
+    private LinkedList<Glyph> hobbyList;
+    private LinkedList<Glyph> majorList;
+    private LinkedList<Glyph> regionList;
+    private LinkedList<Song> songList;
+
+    public Data(String songFileName, String surveyFileName) {
+        
+    }
+
+}
