6da6c6d7c432fe8377c80794324047bce3bc1deb
MattAMarquez <33560698+MattAMarquez@users.noreply.github.com>
2017-11-15 19:33:34 -0500

    Added stuff
    added necessary stuff to build path to allow testing added linkedlisttest

--- src/prj5/LinkedListTest.java
+++ src/prj5/LinkedListTest.java
@@ -1,9 +1,66 @@
 package prj5;
-
-public class LinkedListTest {
-
-    public LinkedListTest() {
-        // TODO Auto-generated constructor stub
+/**
+ * Test class for LinkedList 
+ * @author Matthew Marquez (mmarquez)
+ * @version 2015.11.15
+ *
+ */
+import student.TestCase;
+public class LinkedListTest extends TestCase{
+    private LinkedList<LinkedList> listA;
+    private LinkedList<String> listBinA;
+    private LinkedList<String> listCinA;
+    
+    public void setUp() {
+        listA = new LinkedList<LinkedList>();
+        listBinA = new LinkedList<String>();
+        listCinA = new LinkedList<String>();
+        
+        listBinA.add("Hello");
+        listBinA.add("Fun");
+        listBinA.add("Bye");
+        
+        listCinA.add("Games");
+        listCinA.add("TV");
+        listCinA.add("Comics");
+        
+        listA.add(listBinA);
+        listA.add(listCinA);
     }
-
+    public void testSize() {
+        
+    }
+    public void testClear() {
+        
+    }
+    public void testIsEmpty() {
+        
+    }
+    public void testAddIndex() {
+        
+    }
+    public void testAdd() {
+        
+    }
+    public void testRemove() {
+        
+    }
+    public void testRemoveIndex() {
+        
+    }
+    public void testGet() {
+        
+    }
+    public void testContains() {
+        
+    }
+    public void testToString() {
+        
+    }
+    public void testToArray() {
+        
+    }
+    public void testEquals() {
+        
+    }
 }
