6bff7dad70292473a327e9a44d5ab3c4917cbfc0
MattAMarquez <33560698+MattAMarquez@users.noreply.github.com>
2017-12-06 14:49:00 -0500

    Fixed the ResponseReader
    ResponseReader works properly now

---
+++ src/prj5/DataHandler.java
@@ -0,0 +1,23 @@
+package prj5;
+
+/**
+ * Does the calculations for the data
+ * 
+ * @author Matthew Marquez (mmarquez)
+ * @version 12.06.2017
+ *
+ */
+public class DataHandler {
+    /**
+     * Constructor for DataHandler
+     */
+    private LinkedList<Song> songs;
+    
+    public DataHandler() {
+        
+    }
+    
+
+    
+
+}
--- src/prj5/ResponseReaderTest.java
+++ src/prj5/ResponseReaderTest.java
@@ -1,9 +1,17 @@
 package prj5;
 
-public class ResponseReaderTest {
-
-    public ResponseReaderTest() {
-        // TODO Auto-generated constructor stub
+import java.io.FileNotFoundException;
+import student.TestCase;
+public class ResponseReaderTest extends TestCase {
+    private ResponseReader reader;
+    public void setUp() throws FileNotFoundException {
+        reader = new ResponseReader("MusicSurveyData.csv", "SongList.csv");
     }
-
+    public void testReadResponses() {
+        System.out.println(reader.getResponseList().toString());
+    }
+    public void testReadSongs() {
+        System.out.println(reader.getSongList().toString());
+        assertEquals(reader.getSongList().get(0).getTitle(), "All These Things I've Done");
+    }
 }
