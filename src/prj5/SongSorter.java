c36150436d22b1aade85a7894429e997e768e080
MattAMarquez <33560698+MattAMarquez@users.noreply.github.com>
2017-12-06 21:18:12 -0500

    Fixed SongSorter
    SongSorter should be good now

--- src/prj5/SongSorter.java
+++ src/prj5/SongSorter.java
@@ -1,18 +1,12 @@
 package prj5;
 
 /**
- * Class to organize glyphs
+ * Class to organize song lists
  * 
  * @author Matthew Marquez (mmarquez)
- * @author Eddie Dean
  * @version 2017.11.16
  */
 public class SongSorter {
-    private final LinkedList<Song> sortedTitle;
-    private final LinkedList<Song> sortedArtist;
-    private final LinkedList<Song> sortedGenre;
-    private final LinkedList<Song> sortedYear;
-    private LinkedList<Song> songList;
     private DataHandler handler;
 
 
@@ -20,25 +14,11 @@ public class SongSorter {
      * Basic constructor for GlyphOrganizer
      */
     public SongSorter(DataHandler datahandler) {
+        
         handler = datahandler;
-        songList = handler.getSongList();
-        final LinkedList<Song> sortedTitle = this.sortByTitle(songList);
-        //System.out.println(sortedTitle.toString());
-        this.sortedTitle = sortedTitle;
-        //System.out.println(sortedTitle.toString());
-        final LinkedList<Song> sortedArtist = this.sortByArtist(songList);
-        this.sortedArtist = sortedArtist;
-        //System.out.println(sortedArtist.toString());
-        final LinkedList<Song> sortedGenre = this.sortByGenre(songList);
-        this.sortedGenre = sortedGenre;
-        //System.out.println(sortedGenre.toString());
-        final LinkedList<Song> sortedYear = this.sortByYear(songList);
-        this.sortedYear = sortedYear;
-        //System.out.println(sortedYear.toString());
-        //System.out.println(sortedTitle.toString());
         
     }
-    public final LinkedList<Song> sortByTitle(LinkedList<Song> songList) {
+    public LinkedList<Song> sortByTitle(LinkedList<Song> songList) {
         for (int i=0; i<songList.size(); i++) {
             int min = i;
             for (int j= i+1; j<songList.size(); j++) {
@@ -48,11 +28,9 @@ public class SongSorter {
             }
             songList.swap(min, i);
         }
-        //System.out.println("Title: ");
-        //System.out.println(sortedTitle.toString());
         return songList;
     }
-    public final LinkedList<Song> sortByArtist(LinkedList<Song> songList) {
+    public LinkedList<Song> sortByArtist(LinkedList<Song> songList) {
         for (int i=0; i<songList.size(); i++) {
             int min = i;
             for (int j= i+1; j<songList.size(); j++) {
@@ -63,11 +41,8 @@ public class SongSorter {
             songList.swap(min, i);
         }
         return songList;
-        //System.out.println("Artist: ");
-        //System.out.println(sortedArtist.toString());
-       
     }
-    public final LinkedList<Song> sortByGenre(LinkedList<Song> songList){
+    public LinkedList<Song> sortByGenre(LinkedList<Song> songList){
         for (int i=0; i<songList.size(); i++) {
             int min = i;
             for (int j= i+1; j<songList.size(); j++) {
@@ -78,10 +53,8 @@ public class SongSorter {
             songList.swap(min, i);
         }
         return songList;
-        //System.out.println("Genre: ");
-        //System.out.println(sortedGenre.toString());
     }
-    public final LinkedList<Song> sortByYear(LinkedList<Song> songList) {
+    public LinkedList<Song> sortByYear(LinkedList<Song> songList) {
         for (int i=0; i<songList.size(); i++) {
             int min = i;
             for (int j= i+1; j<songList.size(); j++) {
@@ -92,20 +65,6 @@ public class SongSorter {
             songList.swap(min, i);
         }
         return songList;
-        //System.out.println("Year: ");
-        //System.out.println(sortedYear.toString());
-    }
-    public LinkedList<Song> getSortedTitle(){
-        return sortedTitle;
-    }
-    public LinkedList<Song> getSortedArtist(){
-        return sortedArtist;
-    }
-    public LinkedList<Song> getSortedGenre(){
-        return sortedGenre;
-    }
-    public LinkedList<Song> getSortedYear(){
-        return sortedYear;
     }
     public DataHandler getDataHandler() {
         return handler;
